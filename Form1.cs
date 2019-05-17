22dd16d68ba81a3ea51bb90647767d2310ad2776
shinon0821 <49701829+shinon0821@users.noreply.github.com>
2019-05-17 13:34:38 +0900

    自作プログラム

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,46 @@ namespace fukusyu0517
         {
             InitializeComponent();
         }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "-5";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "3";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "-2";
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "1";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "-1";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label1.Left += int.Parse(textBox1.Text);
+            label1.Top += int.Parse(textBox2.Text);
+        }
     }
 }
